5247233a55b668f429ffce42dba746add039a51b
Markov <roman.markov044@gmail.com>
2025-05-20 15:48:18 +0200

    ft_strlcat.c

--- libft.h
+++ libft.h
@@ -28,7 +28,7 @@ void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
-
+size_t	ft_strlcat(char *dest, const char *src, size_t size)ж
 int		ft_toupper(int c);
 int		ft_tolower(int c);
 char	*ft_strchr(const char *s, int c);
