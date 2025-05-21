7633ea61444383bf1dea8cfe31b4aec8a3de1e24
Roman Markov <rmarkov@c4b10c2.42berlin.de>
2025-05-21 16:47:46 +0200

    ft_atoi.c, added headers

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -1,9 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmarkov <rmarkov@student.42berlin.de>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/21 16:13:40 by rmarkov           #+#    #+#             */
+/*   Updated: 2025/05/21 16:13:42 by rmarkov          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	const unsigned char *s1_p;
-	const unsigned char *s2_p;
+	const unsigned char	*s1_p;
+	const unsigned char	*s2_p;
 
 	s1_p = (const unsigned char *)s1;
 	s2_p = (const unsigned char *)s2;
--- ft_strncmp.c
+++ ft_strncmp.c
@@ -23,7 +23,6 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		i++;
 	}
-
 	return (0);
 }
 
@@ -42,13 +41,14 @@ int	main(void)
 	printf("my func: abc / emply line: %d\n", ft_strncmp(s1, s3, 5));
 	printf("my func: abc / zac: %d\n", ft_strncmp(s1, s5, 3));
 	printf("my func: hello world zac: %d\n", ft_strncmp(s4, s5, 20));
-		
 
 	printf("c func: abc / abc: %d\n", strncmp(s1, s2, 5));
 	printf("c func: abc / emply line: %d\n", strncmp(s1, s3, 5));
 	printf("c func: abc / zac: %d\n", strncmp(s1, s5, 3));
 	printf("c func: hello world zac: %d\n", strncmp(s4, s5, 20));
-	printf("my func: %d\nc func: %d\n", ft_strncmp("abc", "abcd", 20), strncmp("abc", "abcd", 20));
+
+	printf("my func: %d\nc func: %d\n",
+	 ft_strncmp("abc", "abcd", 20), strncmp("abc", "abcd", 20));
 
 	return (0);
 }
