69bc9948e764002ea8d9cbe0e837383332cefe6b
Markov <roman.markov044@gmail.com>
2025-05-25 22:52:32 +0200

    ft_lstlast.c

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: rmarkov <rmarkov@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 19:39:52 by rmarkov           #+#    #+#             */
-/*   Updated: 2025/05/25 22:43:50 by rmarkov          ###   ########.fr       */
+/*   Updated: 2025/05/25 22:46:44 by rmarkov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,5 +62,6 @@ void	ft_putnbr_fd(int n, int fd);
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
 
 #endif
