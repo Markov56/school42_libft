1f52c9c6864ee1f3faf7c6acdc59ddd678b0a1d4
Markov <roman.markov044@gmail.com>
2025-05-25 22:43:09 +0200

    ft_lstadd_front.c

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: rmarkov <rmarkov@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 19:39:52 by rmarkov           #+#    #+#             */
-/*   Updated: 2025/05/25 22:23:45 by rmarkov          ###   ########.fr       */
+/*   Updated: 2025/05/25 22:35:50 by rmarkov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,5 +60,6 @@ void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **lst, t_list *new);
 
 #endif
